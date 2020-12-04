b6b7c3f522a206a99c8d1f2845ce320667054ab8
Martin Hanel <mha@pop-os.localdomain>
2020-12-04 11:32:50 +0100

    package initialization, adding functions and data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Simulated hourly precipitation
+#'
+#' The simulation was done with ALADIN RCM within the CORDEX project
+#'
+#' @format data.table with
+#' \describe{
+#'   \item{data}{date and time}
+#'   \item{p}{precipitation in kg/s}
+#'   \item{forcing}{forcing string id}
+#' }
+"precip_ALADIN_hist"
+
+#' SpatialPolygonsDataFrame representing the Prague area
+"pha"
